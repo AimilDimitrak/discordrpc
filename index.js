7dbae3f2a14414afe7361014770fe4978ba9fe9a
AimilDimitrak <76211331+AimilDimitrak@users.noreply.github.com>
2021-05-08 01:46:01 +0300

    Ready!!!
    This file was only for me but now you can customize it for yourself!

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Discord Dev / Designer",
-state: "Discord Mod / Manager",
+details : "2nd Text shown in presence",
+state: "3rd Text shown in the presence",
 assets : {
-large_image : "discord_large",
-large_text : "DM for the following services!",
-small_image : "discord",
-small_text : "The Master Bot ©2021"
+large_image : "Asset for the Large image",
+large_text : "Large image's text",
+small_image : "Asset for Small Image",
+small_text : "Small image's text"
 },
-buttons : [{label : "My Social Medias" , url : "https://linktr.ee/ADK2618"},{label : "Join Ultimate Community" , url : "https://discord.gg/E2zrAT5vY7"},{label : "Invite The Master",url : "https://discord.com/api/oauth2/authorize?client_id=795669849820889119&permissions=8&scope=bot%20applications.commands"}]
-}
+buttons : [{label : "1st Button Name" , url : "1st Button URL"},{label : "2nd Button Name",url : "2nd Button URL"}]
 })
 })
-client.login({ clientId : "810269127385022464" }).catch(console.error);
+client.login({ clientId : "Your App's Client ID (will have the assets)" }).catch(console.error);
